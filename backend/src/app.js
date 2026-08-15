e83fb3fc236b512bae892a1eb0e61b56bd1269a8
coderhangb <ninjavinh2006@gmail.com>
2026-08-15 16:11:24 +0700

    format and add .env.example

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,9 @@
-
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/authRoute.js");
 require("dotenv").config();
 
 const llmRoutes = require("./routes/llmRoute.js");
-
-
 
 const app = express();
 
@@ -20,8 +17,8 @@ app.use("/public", express.static("src/public"));
 app.use("/api/auth", authRoutes);
 app.use("/api/llm", llmRoutes);
 
-//Bao loi nen tam tat di 
+//Bao loi nen tam tat di
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
-  //connectDB();
+  // connectDB();
 });
