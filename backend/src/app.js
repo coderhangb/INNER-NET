2b9c1cc6a557790604d948aedcd07871a861a613
coderhangb <ninjavinh2006@gmail.com>
2026-09-01 12:37:26 +0700

    add static file serving for frontend build

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 const authRoutes = require("./routes/authRoute.js");
 require("dotenv").config();
 
@@ -14,17 +15,25 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || true,
     credentials: true,
   }),
 );
 app.use(cookieParser());
 app.use(express.json());
-app.use("/public", express.static("src/public"));
+
+// Routes API
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use("/api/auth", authRoutes);
 app.use("/api/llm", llmRoutes);
 
-//Bao loi nen tam tat di
+const distPath = path.join(__dirname, "../../frontend/dist");
+app.use(express.static(distPath));
+
+app.get("/*splat", (req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
   connectDB();
